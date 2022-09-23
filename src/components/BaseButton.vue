04ebd90adab09c623e87f6c20ee22db0f98b7820
Robbert Lokhorst <info@robbertlokhorst.nl>
2022-09-23 21:47:26 +0200

    Add base button

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,54 @@
+<template>
+  <button
+    class="base-button"
+    :class="buttonClasses"
+  >
+    <span class="base-button__span">
+      <slot />
+    </span>
+  </button>
+</template>
+
+<script>
+const availableThemes = ['brand', 'gray']
+const availableSizes = ['sm', 'lg']
+
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: 'brand',
+      validator: value => availableThemes.includes(value)
+    },
+    size: {
+      type: String,
+      default: 'lg',
+      validator: value => availableSizes.includes(value)
+    }
+  },
+  computed: {
+    buttonClasses () {
+      return {
+        [`button--theme-${this.theme}`]: this.theme,
+        [`button--size-${this.size}`]: this.size
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.base-button {
+  background: #475DE5;
+  border-radius: 4px;
+  color: white;
+  font-weight: 500;
+  font-size: 14px;
+  padding: 8.5px 12px;
+  border: 1px solid #475DE5;
+
+  &__span {
+    //
+  }
+}
+</style>
