cc9419bf750dd844fd64b87ba4038c30f2707e0b
Robbert Lokhorst <info@robbertlokhorst.nl>
2022-09-25 16:42:27 +0200

    Add list label

---
+++ src/components/ListLabel.vue
@@ -0,0 +1,13 @@
+<template>
+  <span class="list-label">
+    <slot />
+  </span>
+</template>
+
+<style lang="scss" scoped>
+.list-label {
+  font-weight: 500;
+  font-size: 12px;
+  color: $c-gray-60;
+}
+</style>
